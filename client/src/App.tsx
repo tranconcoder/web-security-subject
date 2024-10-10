f2a3e1c6c692f19d530206f1b7d745aef47a6aed
tvconss <tranconcoder@gmail.com>
2024-10-10 22:41:41 +0700

    apply (global scss): use global scss file to client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,9 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./configs/routes.config";
 
+// Apply global scss style
+import "./assets/scss/global.scss";
+
 function App() {
     return <RouterProvider router={router} />;
 }
